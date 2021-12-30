3cd63e4cd1894d92c293e3381a0bb9a6c5443efd
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2021-12-30 23:12:18 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,14 +1,24 @@
 import pygame
 from pygame import display
 
-
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((800, 500))
+    screen = pygame.display.set_mode((1920, 1080))
     display.set_caption("Монстрики/Монстры")
+    running = True
+    fps = 60
+    clock = pygame.time.Clock()
+    bg = pygame.image.load('bg.png')
+    # pers = pygame.image.load()
 
-    pygame.display.flip()
+    while running:
+        screen.blit(bg, (0, 0))
 
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+        # screen.fill((255, 255, 255))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.update()
+    clock.tick(fps)
+
     pygame.quit()
