14cfdddb9bc124ee162cc0297d520cad279be7b7
Maya1412nez <93309530+Maya1412nez@users.noreply.github.com>
2022-01-04 21:08:16 +0300

    Убран баг

--- Table_file.py
+++ Table_file.py
@@ -1,18 +1,15 @@
 import pygame
 
 
-class Card_table():
+class Card_table(pygame.sprite.Sprite):
     def __init__(self, screen):
+        pygame.sprite.Sprite.__init__(self)
         self.screen = screen
         self.height, self.width = 200, 1600
-
-    def cards(self):
-        self.max_col_csrds = 12
 
     def draw(self):
         # pygame.draw.rect(self.screen, pygame.Color('red'), (0, (800 - self.height), self.width, self.height), 0)
         self.table_surf = pygame.image.load('game_imgs\Table.png')
         self.table_rect = self.table_surf.get_rect(bottomright=(self.width, 800))
         self.screen.blit(self.table_surf, self.table_rect)
-        pygame.display.update()
 
