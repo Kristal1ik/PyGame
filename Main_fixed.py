5f79b1f9a0be0aaa9d0e417faae21d37be3f9b71
Maya1412nez <93309530+Maya1412nez@users.noreply.github.com>
2022-01-04 21:05:39 +0300

    Добавлены классы монстриков и класс их инициализации (рандомизации)

--- Main_fixed.py
+++ Main_fixed.py
@@ -3,7 +3,8 @@ from pygame import display
 from Table_file import Card_table
 from World_file import World, data, bg
 from Arrow_file import Arrow
-from Card_init_file import CardInnit
+from Card_init_file import CardInnit, FirstMonster, SecondMonster, ThirdMonster, \
+    FourthMonster, FifthMonster, SixMonster
 
 if __name__ == '__main__':
     pygame.init()
@@ -19,7 +20,18 @@ if __name__ == '__main__':
 
     world = World(data, tile, screen)
     table = Card_table(screen)
-    cards = CardInnit(screen)
+    cards = CardInnit()
+    Monsters_name_list = cards.give_cards()
+    print(Monsters_name_list)
+
+    First_monster = FirstMonster(all_sprites, screen, Monsters_name_list[0])
+    Second_monster = SecondMonster(all_sprites, screen, Monsters_name_list[1])
+    Third_monster = ThirdMonster(all_sprites, screen, Monsters_name_list[2])
+    Fourth_Monster = FourthMonster(all_sprites, screen, Monsters_name_list[3])
+    Fifth_Monster = FifthMonster(all_sprites, screen, Monsters_name_list[4])
+    Six_Monster = SixMonster(all_sprites, screen, Monsters_name_list[5])
+
+
 
     arrow = Arrow(all_sprites, screen)
     running = True
