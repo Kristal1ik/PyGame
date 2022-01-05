2618802fc7c79a5c4c5be558b5f83dd48ef15fe3
Maya1412nez <93309530+Maya1412nez@users.noreply.github.com>
2022-01-05 13:09:48 +0300

    Объединены Main_fixed и Main. В этом файле активируются все классы

--- Main.py
+++ Main.py
@@ -1,40 +1,67 @@
 import pygame
 from pygame import display
 from Table_file import Card_table
-from World_file import World, data, bg
+from World_file import World, data, back_ground
 from Arrow_file import Arrow
-from pers import Pers
+from Card_init_file import CardInnit, FirstMonster, SecondMonster, ThirdMonster, \
+    FourthMonster, FifthMonster, SixMonster
+from Pers_file import Pers
 
+from Card_init_file import FirstFood, SecondFood, ThirdFood, FourthFood
 
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((1300, 800))
-    display.set_caption("Монстрики/Монстры")
-    running = True
+    display.set_caption('Монстрики/Монстры')
+    size = width, height = 1600, 800
+    screen = display.set_mode(size)
     fps = 60
     tile = 50
+
     clock = pygame.time.Clock()
 
     all_sprites = pygame.sprite.Group()
 
-    # pers = pygame.image.load()
-
+    # создание основных объектов
     world = World(data, tile, screen)
-    cards = Card_table(screen)
-    arrow = Arrow(all_sprites)
+    table = Card_table(screen)
+    cards = CardInnit()
     pers = Pers(all_sprites)
+    # создание списков названий монстров, еды и времени сна
+    Monsters_name_list = cards.get_monster_cards()
+    print(Monsters_name_list)
+    Food_list = cards.get_food_cards()
+    print(Food_list)
+    Time_list = cards.get_sleeping_time()
+    print(Time_list)
+
+    # ---------------создание первых трех карточек МОНСТРИКОВ---------------
+    First_monster = FirstMonster(all_sprites, screen, Monsters_name_list[0], Time_list[0])
+    Second_monster = SecondMonster(all_sprites, screen, Monsters_name_list[1], Time_list[1])
+    Third_monster = ThirdMonster(all_sprites, screen, Monsters_name_list[2], Time_list[2])
+    Fourth_Monster = None
+    Fifth_Monster = None
+    Six_Monster = None
+
+    # ---------------создание первых четырех карточек ЕДЫ---------------
+
+    First_Food = FirstFood(all_sprites, screen, Food_list[0])
+    Second_Food = SecondFood(all_sprites, screen, Food_list[1])
+    Third_Food = None
+    Fourth_Food = None
+
+    arrow = Arrow(all_sprites, screen)
+    running = True
 
     while running:
-        screen.blit(bg, (0, 0))
+        screen.blit(back_ground, (0, 0))
         world.draw()
+        table.draw()
 
-        cards.draw()
-        # screen.fill((255, 255, 255))
+        table = Card_table(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
             key = pygame.key.get_pressed()
             if key:
                 all_sprites.update(key)
@@ -42,6 +69,6 @@ if __name__ == '__main__':
         all_sprites.draw(screen)
         all_sprites.update()
         pygame.display.flip()
-    clock.tick(fps)
 
+    clock.tick(fps)
     pygame.quit()
