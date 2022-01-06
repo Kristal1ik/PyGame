81224013f5c709e6b5cb09f5a08101b16d6d2302
Kristal1ik <korobovtsevaolga@yandex.ru>
2022-01-06 03:07:49 +0300

    упрощение

--- Pers_file.py
+++ Pers_file.py
@@ -3,6 +3,7 @@ import pygame
 
 class Pers(pygame.sprite.Sprite):
     image = pygame.image.load('game_imgs\Charecter.png')
+    image = pygame.transform.scale(image, (50, 90))
     # у персонажа изначальный размер = 100 * 60 пикселей
 
     def __init__(self, group):
@@ -12,14 +13,19 @@ class Pers(pygame.sprite.Sprite):
         self.rect.x = 20
         self.rect.y = 450
 
-    def update(self, *args):
+    def update(self):
         #Изменены клавиши управления, чтобы при работе не было конфликта с классом Arrow.
         #Теперь управление как в большинстве игр - WASD
-        if args and args[0][pygame.K_s]:
-            self.rect.top += 10
-        if args and args[0][pygame.K_w]:
-            self.rect.top -= 10
-        if args and args[0][pygame.K_d]:
-            self.rect.right += 4
-        if args and args[0][pygame.K_a]:
-            self.rect.right -= 4
+
+        #Упращено
+        dx = 0
+        dy = 0
+        args = pygame.key.get_pressed()
+        if args[pygame.K_s]:
+            self.rect.top += 5
+        if args[pygame.K_SPACE]:
+            self.rect.top -= 5
+        if args[pygame.K_d]:
+            self.rect.right += 5
+        if args[pygame.K_a]:
+            self.rect.right -= 5
