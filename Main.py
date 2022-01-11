6d9da5a86395b6e8f873683c01a7a4500f5a7118
Maya1412nez <93309530+Maya1412nez@users.noreply.github.com>
2022-01-11 22:21:57 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame import display
 from Table_file import Card_table
-from World_file import World, data, back_ground
+from World_file import World, back_ground, first_level_data, second_level_data
 from Arrow_file import Arrow
 from Card_init_file import CardInnit, FirstMonster, SecondMonster, ThirdMonster, \
     FourthMonster, FifthMonster, SixMonster
@@ -15,14 +15,15 @@ if __name__ == '__main__':
     size = width, height = 1600, 800
     screen = display.set_mode(size)
     fps = 60
-    tile = 50
+    tile_x, tile_y = 70, 50
 
     clock = pygame.time.Clock()
 
     all_sprites = pygame.sprite.Group()
 
     # создание основных объектов
-    world = World(data, tile, screen)
+    world = World(first_level_data, tile_x, tile_y, screen)
+    num_of_data = 1
     table = Card_table(screen)
     cards = CardInnit()
     pers = Pers(all_sprites)
@@ -59,15 +60,26 @@ if __name__ == '__main__':
 
         table = Card_table(screen)
 
+        # смена уровней. пока костыльно
+        pers_x = pers.get_cords()
+        num_of_data = world.get_num_of_data()
+        if num_of_data == 2 and pers_x >= 1500:
+            world = World(second_level_data, tile_x, tile_y, screen)
+        if num_of_data == 1 and pers_x <= 100:
+            world = World(first_level_data, tile_x, tile_y, screen)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             key = pygame.key.get_pressed()
             if key:
                 all_sprites.update()
+                # print(pers_x)
+                # обновление мира
+                world.updating_world(pers_x, num_of_data)
 
 
-        print(all_sprites)
+        #print(all_sprites)
         all_sprites.draw(screen)
         all_sprites.update()
         pygame.display.flip()
