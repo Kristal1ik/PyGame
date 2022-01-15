ad20f0709950f1efec29293c36490869ef14a38b
Kristal1ik <korobovtsevaolga@yandex.ru>
2022-01-15 22:57:16 +0300

    подключение стартового окна, наконец-то

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pygame
-from Try import Particle
+# from start_window import Start_window, buttons_rect
 from pygame import display
+from Button import Button
 from Table_file import Card_table
 from World_file import World, back_ground, first_level_data, second_level_data
 from Arrow_file import Arrow
@@ -13,16 +14,25 @@ from Card_init_file import FirstFood, SecondFood, ThirdFood, FourthFood
 if __name__ == '__main__':
     pygame.init()
     display.set_caption('Монстрики/Монстры')
+
     size = width, height = 1600, 800
+
     screen = display.set_mode(size)
     fps = 25
     tile_x, tile_y = 70, 50
+    menu = True
 
     clock = pygame.time.Clock()
 
     all_sprites = pygame.sprite.Group()
 
     # создание основных объектов
+    image_start = pygame.image.load('game_imgs/Start.png')
+    image_start = pygame.transform.scale(image_start, (484, 131))
+    bg = pygame.image.load('game_imgs\Back.jpg')
+    bg = pygame.transform.scale(bg, (width, height))
+
+    button = Button(width, height, image_start, screen)
     world = World(first_level_data, tile_x, tile_y, screen)
     num_of_data = 1
     table = Card_table(screen)
@@ -56,34 +66,38 @@ if __name__ == '__main__':
 
     while running:
         clock.tick(fps)
-        screen.blit(back_ground, (0, 0))
-        world.draw()
-        table.draw()
+        if menu:
+            screen.blit(bg, (0, 0))
+            if button.draw():
+                menu = False
+        else:
+            screen.blit(back_ground, (0, 0))
 
-        table = Card_table(screen)
+            world.draw()
+            table.draw()
+            table = Card_table(screen)
 
-        # смена уровней. пока костыльно
-        pers_x = pers.get_cords()
-        num_of_data = world.get_num_of_data()
-        if num_of_data == 2 and pers_x >= 1500:
-            world = World(second_level_data, tile_x, tile_y, screen)
-        if num_of_data == 1 and pers_x <= 100:
-            world = World(first_level_data, tile_x, tile_y, screen)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            # смена уровней. пока костыльно
+            pers_x = pers.get_cords()
+            num_of_data = world.get_num_of_data()
+            if num_of_data == 2 and pers_x >= 1500:
+                world = World(second_level_data, tile_x, tile_y, screen)
+            if num_of_data == 1 and pers_x <= 100:
+                world = World(first_level_data, tile_x, tile_y, screen)
             key = pygame.key.get_pressed()
             if key:
                 all_sprites.update()
                 # print(pers_x)
                 # обновление мира
                 world.updating_world(pers_x, num_of_data)
+            # print(all_sprites)
+            all_sprites.draw(screen)
+            all_sprites.update()
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        #print(all_sprites)
-        all_sprites.draw(screen)
-        all_sprites.update()
         pygame.display.flip()
 
     clock.tick(fps)
