efb6d8818a3e3dd98fd79397cf3f5707dd1cf495
Kristal1ik <korobovtsevaolga@yandex.ru>
2022-01-17 16:04:20 +0300

    Класс кристаллов

--- Kristall.py
+++ Kristall.py
@@ -5,8 +5,9 @@ class Kristall(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load('game_imgs/Kristall1.png')
-        self.image = pygame.transform.scale(img, (x * 2, y * 2))
+        self.image = pygame.transform.scale(img, (70 // 2, 50 // 2))
         self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
 
     def get_rect(self):
         return self.rect.x, self.rect.y
